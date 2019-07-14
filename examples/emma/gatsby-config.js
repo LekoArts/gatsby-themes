f092dc4faf26073582a892a6b4e13c98f2ef514c
LekoArts <lekoarts@gmail.com>
2019-07-14 17:42:45 +0200

    add some plugins to the emma example

--- examples/emma/gatsby-config.js
+++ examples/emma/gatsby-config.js
@@ -1,8 +1,45 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
 module.exports = {
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emma`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Emma - @lekoarts/gatsby-theme-emma`,
+        short_name: `Emma`,
+        description: `Minimalistic bright portfolio with full-width grid and large images`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#b83280`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
   ],
 }
