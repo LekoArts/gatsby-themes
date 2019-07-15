b4bf9fc71c3bec282fb5d019cd455a7550255258
Lennart <lekoarts@gmail.com>
2019-07-15 18:13:06 +0200

    Also include the existing tailwind styles into headings

--- themes/gatsby-theme-emma/src/gatsby-plugin-theme-ui/index.js
+++ themes/gatsby-theme-emma/src/gatsby-plugin-theme-ui/index.js
@@ -54,24 +54,30 @@ export default {
       "--x-height-multiplier": 0.35,
     },
     h1: {
+      ...tailwind.styles.h1,
       fontSize: [5, 6],
       mt: 2,
     },
     h2: {
+      ...tailwind.styles.h2,
       fontSize: [4, 5],
       mt: 2,
     },
     h3: {
+      ...tailwind.styles.h3,
       fontSize: [3, 4],
       mt: 3,
     },
     h4: {
+      ...tailwind.styles.h4,
       fontSize: [2, 3],
     },
     h5: {
+      ...tailwind.styles.h5,
       fontSize: [1, 2],
     },
     h6: {
+      ...tailwind.styles.h6,
       fontSize: 1,
       mb: 2,
     },
