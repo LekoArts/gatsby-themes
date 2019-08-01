54ca0efa28de57da017405d8399116abd3228131
LekoArts <lekoarts@gmail.com>
2019-08-02 00:21:03 +0200

    remove sudo command from dev sh

--- scripts/e2e-dev.sh
+++ scripts/e2e-dev.sh
@@ -2,6 +2,4 @@
 
 EXAMPLE_NAME=$1
 
-command -v sudo &&
-
 yarn concurrently --kill-others "yarn tsc:compile:watch" "yarn ssat 'yarn workspace $EXAMPLE_NAME develop:cypress' http://localhost:8000 cy:open"
