6cc50b3c2c4443256ae2d68bf7e54bc8233b1bd4
LekoArts <lekoarts@gmail.com>
2019-08-01 19:59:41 +0200

    changes to script

--- scripts/e2e-build.sh
+++ scripts/e2e-build.sh
@@ -2,8 +2,10 @@
 
 EXAMPLE_NAME=$1
 
-command -v sudo &&
+command -v sudo && # The default node executor does need that
 
-yarn tsc:compile &&
-yarn workspace "$EXAMPLE_NAME" build:cypress &&
-yarn ssat "yarn workspace $EXAMPLE_NAME serve" http://localhost:9000 "yarn cy:run --spec 'cypress/e2e/build/$EXAMPLE_NAME.js'"
+chmod +x ./node_modules/.bin/gatsby && # Sometimes necessary
+
+yarn tsc:compile && # Compile all files in e2e to e2e/build
+yarn workspace "$EXAMPLE_NAME" build:cypress && # run "gatsby build" in the example
+yarn ssat "yarn workspace $EXAMPLE_NAME serve" http://localhost:9000 "yarn cy:run --spec 'cypress/e2e/build/$EXAMPLE_NAME.js'" # run "gatsby serve" and "cypress run"
