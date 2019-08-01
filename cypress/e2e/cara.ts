a0bbfa69b7802c6e709048acc8deb434c3f5d1b6
LekoArts <lekoarts@gmail.com>
2019-08-02 00:10:42 +0200

    finish cara tests

--- cypress/e2e/cara.ts
+++ cypress/e2e/cara.ts
@@ -6,6 +6,23 @@ describe(`gatsby-theme-cara`, () => {
   beforeEach(() => {
     cy.visit(`/`).waitForRouteChange()
   })
+  it(`should render hero content`, () => {
+    cy.getByText(/hi, i'm jane doe/i)
+  })
+  it(`should render project content`, () => {
+    cy.getByText(/freiheit/i).getByText(/this project is my entry to adobe's #challengeyourperspective contest./i)
+  })
+  it(`should render project cards with links`, () => {
+    cy.getByText(/freiheit/i)
+      .parent()
+      .should(`have.attr`, `href`, `https://www.behance.net/gallery/58937147/Freiheit`)
+  })
+  it(`should render about content`, () => {
+    cy.getByText(/winky face./i)
+  })
+  it(`should render contact content`, () => {
+    cy.getByText(/Dribbble/i)
+  })
   it(`should render the light/dark mode toggle`, () => {
     cy.getByLabelText(/toggle dark mode/i)
   })
