f43cca255a716fbb492a8bd313fe3f08319c66cf
Lennart <lekoarts@gmail.com>
2019-08-03 11:40:46 +0200

    chore(gatsby-theme-cara): Change SEO meta tag

--- themes/gatsby-theme-cara/src/components/seo.tsx
+++ themes/gatsby-theme-cara/src/components/seo.tsx
@@ -55,7 +55,7 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
       <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:image:alt" content={seo.description} />
       <meta name="twitter:creator" content={author} />
-      <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-emma" />
+      <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-cara" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
