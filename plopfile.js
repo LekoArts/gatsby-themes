6ee385185236fe0c47329cab2d6a2e659f8a2b13
LekoArts <lekoarts@gmail.com>
2019-08-05 14:39:15 +0200

    Modify templates, fix cypress path, add readme-name

--- plopfile.js
+++ plopfile.js
@@ -6,6 +6,11 @@ const prompt = [
   },
   {
     type: `input`,
+    name: `readme-name`,
+    message: `Name for the README title, alt tags of images and siteMetadata`,
+  },
+  {
+    type: `input`,
     name: `starter-name`,
     message: `Name of the respective starter project (if already existing use the name after "LekoArts/")`,
   },
@@ -14,42 +19,42 @@ const prompt = [
 const themeActions = [
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/.npmignore`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/.npmignore`,
     templateFile: `plop-templates/theme/.npmignore.hbs`,
   },
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/gatsby-config.js`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/gatsby-config.js`,
     templateFile: `plop-templates/theme/gatsby-config.js.hbs`,
   },
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/index.js`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/index.js`,
     templateFile: `plop-templates/theme/index.js.hbs`,
   },
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/package.json`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/package.json`,
     templateFile: `plop-templates/theme/package.json.hbs`,
   },
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/README.md`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/README.md`,
     templateFile: `plop-templates/theme/README.md.hbs`,
   },
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/src/components/seo.tsx`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/src/components/seo.tsx`,
     templateFile: `plop-templates/theme/seo.tsx.hbs`,
   },
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/src/gatsby-plugin-theme-ui/index.js`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/src/gatsby-plugin-theme-ui/index.js`,
     templateFile: `plop-templates/theme/theme-ui.js.hbs`,
   },
   {
     type: `add`,
-    path: `themes/{{kebabCase name}}/src/hooks/use-site-metadata.tsx`,
+    path: `themes/gatsby-theme-{{kebabCase name}}/src/hooks/use-site-metadata.tsx`,
     templateFile: `plop-templates/theme/use-site-metadata.tsx.hbs`,
   },
 ]
@@ -83,7 +88,7 @@ const exampleActions = [
   {
     type: `add`,
     path: `cypress/e2e/{{kebabCase name}}.ts`,
-    templateFile: `plop-templates/theme/cypress-test.ts.hbs`,
+    templateFile: `plop-templates/cypress-test.ts.hbs`,
   },
 ]
 
