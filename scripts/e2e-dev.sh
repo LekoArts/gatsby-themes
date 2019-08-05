bb526c5c36cd885545092faf95c81898af7b7a70
LekoArts <lekoarts@gmail.com>
2019-08-05 13:16:46 +0200

    chore: Make the start-server-and-test script more verbose

--- scripts/e2e-dev.sh
+++ scripts/e2e-dev.sh
@@ -2,4 +2,4 @@
 
 EXAMPLE_NAME=$1
 
-yarn concurrently --kill-others "yarn tsc:compile:watch" "yarn ssat 'yarn workspace $EXAMPLE_NAME develop:cypress' http://localhost:8000 cy:open"
+yarn concurrently --kill-others "yarn tsc:compile:watch" "yarn start-server-and-test 'yarn workspace $EXAMPLE_NAME develop:cypress' http://localhost:8000 cy:open"
