8d41c4350eaac521d7cc98de804957f7a4e306c8
LekoArts <lekoarts@gmail.com>
2019-08-05 13:20:26 +0200

    chore: Change ssat from previous commit

--- scripts/e2e-build.sh
+++ scripts/e2e-build.sh
@@ -13,6 +13,6 @@ chmod +x ./node_modules/.bin/gatsby && # Sometimes necessary
 
 yarn tsc:compile && # Compile all files in e2e to e2e/build
 yarn workspace "$EXAMPLE_NAME" build:cypress && # run "gatsby build" in the example
-yarn ssat "yarn workspace $EXAMPLE_NAME serve" http://localhost:9000 "yarn cy:run --spec 'cypress/e2e/build/$EXAMPLE_NAME.js'" && # run "gatsby serve" and "cypress run"
+yarn start-server-and-test "yarn workspace $EXAMPLE_NAME serve" http://localhost:9000 "yarn cy:run --spec 'cypress/e2e/build/$EXAMPLE_NAME.js'" && # run "gatsby serve" and "cypress run"
 
 echo "E2E Script ended"
