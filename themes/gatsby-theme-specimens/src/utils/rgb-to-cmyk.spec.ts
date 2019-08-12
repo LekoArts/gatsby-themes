773474ec92a8c9493735c867d23261407592f4c3
LekoArts <lekoarts@gmail.com>
2019-08-12 22:26:01 +0200

    the tests FINALLY work

--- themes/gatsby-theme-specimens/src/utils/rgb-to-cmyk.spec.ts
+++
@@ -1,7 +0,0 @@
-import RGBToCMYK from "./rgb-to-cmyk"
-
-describe(`rgb-to-cymk`, () => {
-  test(`should convert white correctly`, () => {
-    expect(RGBToCMYK(255, 255, 255)).toBe({ C: 0, M: 0, Y: 0, K: 100 })
-  })
-})
