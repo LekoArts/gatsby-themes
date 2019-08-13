013331deeebae43e0d385ee0b646d42b4b15cc8e
LekoArts <lekoarts@gmail.com>
2019-08-14 00:54:15 +0200

    working filter styles

--- themes/gatsby-theme-specimens/src/utils/filter-styles.ts
+++ themes/gatsby-theme-specimens/src/utils/filter-styles.ts
@@ -1,12 +1,3 @@
-const filterStyles = ({ input, allowed }) =>
-  Object.keys(input)
-    .filter(key => allowed.includes(key))
-    .reduce(
-      (obj, key) => ({
-        ...obj,
-        [key]: input[key],
-      }),
-      {}
-    )
+const filterStyles = ({ input, allowed }) => allowed.reduce((obj, key) => ({ ...obj, [key]: input[key] }), {})
 
 export default filterStyles
