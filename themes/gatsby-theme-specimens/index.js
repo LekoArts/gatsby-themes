375f50a318e6c667afbe7069f712fcbd0d631afa
LekoArts <lekoarts@gmail.com>
2019-08-14 16:52:10 +0200

    download specimen

--- themes/gatsby-theme-specimens/index.js
+++ themes/gatsby-theme-specimens/index.js
@@ -2,10 +2,11 @@ import Audio from "./src/components/audio"
 import Alert from "./src/components/alert"
 import ColorRow from "./src/components/color-row"
 import ColorSwatch from "./src/components/color-swatch"
+import Download from "./src/components/download"
 import filterStyles from "./src/utils/filter-styles"
 import normalizeThemeUI from "./src/utils/normalize-theme-ui"
 import Palette from "./src/components/palette"
 import Typography from "./src/components/typography"
 import Video from "./src/components/video"
 
-export { Audio, Alert, ColorRow, ColorSwatch, filterStyles, normalizeThemeUI, Palette, Typography, Video }
+export { Audio, Alert, ColorRow, ColorSwatch, Download, filterStyles, normalizeThemeUI, Palette, Typography, Video }
