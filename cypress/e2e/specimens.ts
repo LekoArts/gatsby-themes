ea9dbd522046e25de8aaafbd1d2a3272dba6c63b
LekoArts <lekoarts@gmail.com>
2019-08-16 20:05:43 +0200

    remove empty e2e test

--- cypress/e2e/specimens.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="../support/index" />
-/// <reference types="cypress" />
-/// <reference types="@testing-library/cypress/typings" />
-
-describe(`gatsby-theme-specimens`, () => {
-  it(`should work`, () => {
-    cy.visit(`/`).assertRoute(`/`)
-  })
-})
