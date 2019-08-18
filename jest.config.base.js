05c9d689f2d6f8b2560969b9e2d3fffa97d657f0
LekoArts <lekoarts@gmail.com>
2019-08-18 12:02:07 +0200

    remove cleanupaftereach as it's not longer needed

--- jest.config.base.js
+++ jest.config.base.js
@@ -4,7 +4,7 @@ module.exports = {
   moduleNameMapper: {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `../../jest/__mocks__/file-mock.js`,
   },
-  setupFilesAfterEnv: [`@testing-library/react/cleanup-after-each`, `../../jest/loadershim.js`],
+  setupFilesAfterEnv: [`../../jest/loadershim.js`],
   globals: {
     __PATH_PREFIX__: ``,
   },
