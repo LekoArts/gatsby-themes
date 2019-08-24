48277dd1c94635b696864309f446431d8186e760
LekoArts <lekoarts@gmail.com>
2019-08-24 12:09:08 +0200

    change footers

--- themes/gatsby-theme-cara/src/components/footer.tsx
+++ themes/gatsby-theme-cara/src/components/footer.tsx
@@ -20,14 +20,29 @@ const Footer = () => {
       </button>
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       <br />
-      <Flex sx={{ justifyContent: `center`, alignItems: `center`, mt: 3 }}>
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
         <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_w30.png" alt="LekoArts Logo" />
         {` `}
         <Styled.a
-          sx={{ color: `text`, fontWeight: `semibold`, ml: 2 }}
-          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-cara"
+          aria-label="Link to the theme's GitHub repository"
+          sx={{ ml: 2 }}
+          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-emma"
         >
-          Theme by LekoArts
+          Theme
+        </Styled.a>
+        <div sx={{ mx: 1 }}>by</div>
+        {` `}
+        <Styled.a aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
+          LekoArts
         </Styled.a>
       </Flex>
     </ThemeFooter>
