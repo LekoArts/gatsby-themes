3d076ec2dc4a81301de5db85d834f38049eaea1e
LekoArts <lekoarts@gmail.com>
2019-09-02 17:19:23 +0200

    got -core working

--- themes/gatsby-theme-emilia-core/gatsby-config.js
+++ themes/gatsby-theme-emilia-core/gatsby-config.js
@@ -1,13 +1,37 @@
-module.exports = themeOptions => ({
-  siteMetadata: {
-    siteTitle: `Emilia`,
-    siteTitleAlt: `Emilia - @lekoarts/gatsby-theme-emilia-core`,
-    siteHeadline: `Emilia - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://emilia-core.lekoarts.de`,
-    siteDescription: `Stub description for emilia-core`,
-    siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
-  },
-  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-theme-ui`],
-})
+const withDefaults = require(`./utils/default-options`)
+
+module.exports = themeOptions => {
+  const options = withDefaults(themeOptions)
+  const { mdx = true } = themeOptions
+
+  return {
+    siteMetadata: {
+      siteTitle: `Emilia`,
+      siteTitleAlt: `Emilia - @lekoarts/gatsby-theme-emilia-core`,
+      siteHeadline: `Emilia - Gatsby Theme from @lekoarts`,
+      siteUrl: `https://emilia.lekoarts.de`,
+      siteDescription: `Stub description for emilia-core`,
+      siteLanguage: `en`,
+      siteImage: `/banner.jpg`,
+      author: `@lekoarts_de`,
+      basePath: options.basePath,
+      projectsPath: options.projectsPath,
+    },
+    plugins: [
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: options.projectsPath,
+          path: options.projectsPath,
+        },
+      },
+      mdx && {
+        resolve: `gatsby-plugin-mdx`,
+        options: {},
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-typescript`,
+    ].filter(Boolean),
+  }
+}
