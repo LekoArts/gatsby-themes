2582b313b75f62f7b27273e20a3c4bd18af2fdce
LekoArts <lekoarts@gmail.com>
2019-09-04 12:08:48 +0200

    guard against window error on build

--- themes/gatsby-theme-emilia/src/utils/useMedia.ts
+++ themes/gatsby-theme-emilia/src/utils/useMedia.ts
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react"
 
 /* eslint-disable */
 export default function useMedia(queries: string[], values: number[], defaultValue: number) {
-  const match = () => values[queries.findIndex(q => matchMedia(q).matches)] || defaultValue
+  const match = () => values[queries.findIndex(q => {
+    if (typeof window !== `undefined`) {
+      return matchMedia(q).matches
+    }
+  })] || defaultValue
   const [value, set] = useState(match)
   useEffect(() => {
     const handler = () => set(match)
-    window.addEventListener(`resize`, handler)
+    if (typeof window !== `undefined`) {
+      window.addEventListener(`resize`, handler)
+    }
     return () => window.removeEventListener(`resize`, handler)
   }, [])
   return value
