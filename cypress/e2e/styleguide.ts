0438ae1ef569b9f229196a9bb83da1fe9f1a969c
LekoArts <lekoarts@gmail.com>
2019-12-31 16:23:04 +0100

    add cypress tests

--- cypress/e2e/styleguide.ts
+++ cypress/e2e/styleguide.ts
@@ -3,7 +3,23 @@
 /// <reference types="@types/testing-library__cypress" />
 
 describe(`gatsby-theme-styleguide`, () => {
-  it(`should work`, () => {
-    cy.visit(`/`).assertRoute(`/`)
+  it(`should render the title`, () => {
+    cy.visit(`/`)
+      .assertRoute(`/`)
+      .findByText(/Style Guide/i)
+  })
+  it(`should render the colors section`, () => {
+    cy.visit(`/`)
+      .findByText(/Colors/i)
+      .findByText(/danger/i)
+  })
+  it(`should render the footer`, () => {
+    cy.visit(`/`)
+      .findByText(`@lekoarts/gatsby-theme-styleguide`)
+      .should(
+        `have.attr`,
+        `href`,
+        `https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-styleguide`
+      )
   })
 })
