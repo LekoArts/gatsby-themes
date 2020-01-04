f676d622f97a04faf72c7e1fc71472ffa043f138
LekoArts <lekoarts@gmail.com>
2020-01-04 17:41:24 +0100

    fix some theme ui heading issue

--- themes/gatsby-theme-minimal-blog/src/components/title.tsx
+++ themes/gatsby-theme-minimal-blog/src/components/title.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import React from "react"
 import { jsx } from "theme-ui"
-import { Heading } from "@theme-ui/components"
+import { Box } from "@theme-ui/components"
 
 type TitleProps = {
   children?: React.ReactNode
@@ -25,9 +25,13 @@ const Title = ({ text, children, as = `h2`, className }: TitleProps) => (
       display: `flex`,
     }}
   >
-    <Heading as={as} sx={{ fontWeight: `medium`, fontSize: [3, 4], fontFamily: `heading` }} className={className}>
+    <Box
+      as={as}
+      sx={{ fontWeight: `medium`, fontSize: [3, 4], fontFamily: `heading`, lineHeight: `heading`, color: `heading` }}
+      className={className}
+    >
       {text}
-    </Heading>
+    </Box>
     <div
       sx={{
         color: `secondary`,
