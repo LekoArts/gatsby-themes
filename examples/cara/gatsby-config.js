68c11f3a631420d8168efa5c1b003d2ea0b4a815
LekoArts <lekoarts@gmail.com>
2020-01-23 16:54:57 +0100

    Add example to config

--- examples/cara/gatsby-config.js
+++ examples/cara/gatsby-config.js
@@ -4,11 +4,15 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // Of course you can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
     siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
       options: {},
     },
     {
