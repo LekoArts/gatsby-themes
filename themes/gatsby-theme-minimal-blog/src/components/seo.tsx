df781854e78576f798e7b2deee56892a7e100eba
Jesse Hallett <jesse@sitr.us>
2020-01-25 15:54:25 -0500

    fix(gatsby-theme-minimal-blog): remove icon link tags from `SEO` component
    gatsby-plugin-manifest injects its own icon links - setting links in
    `SEO` prevents the manifest settings from working.

--- themes/gatsby-theme-minimal-blog/src/components/seo.tsx
+++ themes/gatsby-theme-minimal-blog/src/components/seo.tsx
@@ -56,9 +56,6 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
       <meta name="twitter:image:alt" content={seo.description} />
       <meta name="twitter:creator" content={author} />
       <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-minimal-blog" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       {children}
     </Helmet>
   )
