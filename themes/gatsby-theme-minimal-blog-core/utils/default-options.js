b7e63007567e365ad7928999ef2441c6fe8eaaf5
danigb <danigb@gmail.com>
2020-02-21 23:27:54 +0100

    Fix format

--- themes/gatsby-theme-minimal-blog-core/utils/default-options.js
+++ themes/gatsby-theme-minimal-blog-core/utils/default-options.js
@@ -1,23 +1,23 @@
 module.exports = themeOptions => {
-  const basePath = themeOptions.basePath || `/`;
-  const blogPath = themeOptions.blogPath || `/blog`;
-  const postsPath = themeOptions.postsPath || `content/posts`;
-  const pagesPath = themeOptions.pagesPath || `content/pages`;
-  const tagsPath = themeOptions.tagsPath || `/tags`;
+  const basePath = themeOptions.basePath || `/`
+  const blogPath = themeOptions.blogPath || `/blog`
+  const postsPath = themeOptions.postsPath || `content/posts`
   const additionalPostsFolders = themeOptions.additionalPostsFolders || [];
-  const externalLinks = themeOptions.externalLinks || [];
-  const navigation = themeOptions.navigation || [];
-  const showLineNumbers = themeOptions.showLineNumbers || true;
+  const pagesPath = themeOptions.pagesPath || `content/pages`
+  const tagsPath = themeOptions.tagsPath || `/tags`
+  const externalLinks = themeOptions.externalLinks || []
+  const navigation = themeOptions.navigation || []
+  const showLineNumbers = themeOptions.showLineNumbers || true
 
   return {
     basePath,
     blogPath,
     postsPath,
+    additionalPostsFolders,
     pagesPath,
     tagsPath,
-    additionalPostsFolders,
     externalLinks,
     navigation,
-    showLineNumbers
-  };
-};
+    showLineNumbers,
+  }
+}
