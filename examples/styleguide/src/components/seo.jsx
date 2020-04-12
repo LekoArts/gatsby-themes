9c1562eac4d385a03481c80b69635d87589e5e36
LekoArts <lekoarts@gmail.com>
2020-04-12 23:08:31 +0200

    fix: Use withPrefix helper in SEO component

--- examples/styleguide/src/components/seo.jsx
+++ examples/styleguide/src/components/seo.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Helmet from "react-helmet"
+import { withPrefix } from "gatsby"
 import useSiteMetadata from "../hooks/use-site-metadata"
 
 const SEO = ({ title = ``, description = false, pathname = false, image = false, children = null }) => {
@@ -40,9 +41,9 @@ const SEO = ({ title = ``, description = false, pathname = false, image = false,
       <meta name="twitter:image:alt" content={seo.description} />
       <meta name="twitter:creator" content={author} />
       <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-minimal-blog" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href={withPrefix(`/favicon-32x32.png`)} />
+      <link rel="icon" type="image/png" sizes="16x16" href={withPrefix(`/favicon-16x16.png`)} />
+      <link rel="apple-touch-icon" sizes="180x180" href={withPrefix(`/apple-touch-icon.png`)} />
       {children}
     </Helmet>
   )
