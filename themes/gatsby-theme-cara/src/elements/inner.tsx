c0d1f1685d29f4fde0477ff9fef39ec6b499f619
LekoArts <lekoarts@gmail.com>
2020-04-26 21:32:02 +0200

    improve cara

--- themes/gatsby-theme-cara/src/elements/inner.tsx
+++ themes/gatsby-theme-cara/src/elements/inner.tsx
@@ -1,8 +1,6 @@
 /** @jsx jsx */
 import React from "react"
-import { MDXProvider } from "@mdx-js/react"
 import { jsx } from "theme-ui"
-import components from "../gatsby-plugin-theme-ui/components"
 
 type InnerProps = {
   className?: string
@@ -10,11 +8,9 @@ type InnerProps = {
 }
 
 const Inner = ({ className, children }: InnerProps) => (
-  <MDXProvider components={components}>
-    <div sx={{ width: [`full`, `full`, `full`, `full`, `full`, `2/3`], textAlign: `left` }} className={className}>
-      {children}
-    </div>
-  </MDXProvider>
+  <div sx={{ width: [`full`, `full`, `full`, `full`, `full`, `2/3`], textAlign: `left` }} className={className}>
+    {children}
+  </div>
 )
 
 export default Inner
