10b0ec5ca4868f07855d60e86bb38b9c65072eb9
Bruce Ingalls <bruce.ingalls@gmail.com>
2021-08-27 21:23:15 -0400

    upgraded package.json & fixed warnings

--- themes/gatsby-theme-emma/src/components/layout.tsx
+++ themes/gatsby-theme-emma/src/components/layout.tsx
@@ -15,39 +15,41 @@ const Layout = ({ children, className = `` }: LayoutProps) => {
 
   return (
     <React.Fragment>
-      <Global
-        styles={(theme) => ({
-          "*": {
-            boxSizing: `inherit`,
-          },
-          html: {
-            WebkitTextSizeAdjust: `100%`,
-          },
-          img: {
-            borderStyle: `none`,
-          },
-          pre: {
-            fontFamily: `monospace`,
-            fontSize: `1em`,
-          },
-          "[hidden]": {
-            display: `none`,
-          },
-          "::selection": {
-            backgroundColor: theme.colors.text,
-            color: theme.colors.background,
-          },
-          a: {
-            transition: `all 0.3s ease-in-out`,
-          },
-        })}
-      />
-      <Seo />
-      <Header meta={meta} nav={nav} />
-      <Box as="main" variant="layout.main" className={className}>
-        {children}
-      </Box>
-      <Footer />
+      <React.StrictMode>
+        <Global
+          styles={(theme) => ({
+            "*": {
+              boxSizing: `inherit`,
+            },
+            html: {
+              WebkitTextSizeAdjust: `100%`,
+            },
+            img: {
+              borderStyle: `none`,
+            },
+            pre: {
+              fontFamily: `monospace`,
+              fontSize: `1em`,
+            },
+            "[hidden]": {
+              display: `none`,
+            },
+            "::selection": {
+              backgroundColor: theme.colors.text,
+              color: theme.colors.background,
+            },
+            a: {
+              transition: `all 0.3s ease-in-out`,
+            },
+          })}
+        />
+        <Seo />
+        <Header meta={meta} nav={nav} />
+        <Box as="main" variant="layout.main" className={className}>
+          {children}
+        </Box>
+        <Footer />
+      </React.StrictMode>
     </React.Fragment>
   )
 }
