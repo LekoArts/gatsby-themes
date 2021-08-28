97d39e9a2103236bb045fad24257e8418754aff7
Bruce Ingalls <bruce.ingalls@gmail.com>
2021-08-27 20:01:10 -0400

    added missing 404

---
+++ examples/emma/pages/404.jsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { Container, Themed, Link as TLink } from "theme-ui"
+import Header from "@lekoarts/gatsby-theme-emilia/src/components/header"
+import Layout from "@lekoarts/gatsby-theme-emilia/src/components/layout"
+import Seo from "@lekoarts/gatsby-theme-emilia/src/components/seo"
+
+const NotFound = () => (
+  <Layout>
+    <Seo title="404 - Not found" />
+    <Header />
+    <Container>
+      <Themed.p>
+        Oh, no!
+        <br />
+        You found a page that doesn't exist.{` `}
+        <TLink as={Link} to="/">
+          To you want to visit the homepage?
+        </TLink>
+        .
+      </Themed.p>
+    </Container>
+  </Layout>
+)
+
+export default NotFound
