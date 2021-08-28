f142ee7bdc2b05e089a47c2ebb8d6411673cda93
Bruce Ingalls <bruce.ingalls@gmail.com>
2021-08-27 21:29:36 -0400

    fixed 404 links to emma

--- examples/emma/src/pages/404.jsx
+++ examples/emma/src/pages/404.jsx
@@ -1,26 +1,33 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { Container, Themed, Link as TLink } from "theme-ui"
-import Header from "@lekoarts/gatsby-theme-emilia/src/components/header"
-import Layout from "@lekoarts/gatsby-theme-emilia/src/components/layout"
-import Seo from "@lekoarts/gatsby-theme-emilia/src/components/seo"
+import Header from "@lekoarts/gatsby-theme-emma/src/components/header"
+import Layout from "@lekoarts/gatsby-theme-emma/src/components/layout"
+import Seo from "@lekoarts/gatsby-theme-emma/src/components/seo"
 
-const NotFound = () => (
-  <Layout>
-    <Seo title="404 - Not found" />
-    <Header />
-    <Container>
-      <Themed.p>
-        Oh, no!
-        <br />
-        You found a page that doesn't exist.{` `}
-        <TLink as={Link} to="/">
-          To you want to visit the homepage?
-        </TLink>
-        .
-      </Themed.p>
-    </Container>
-  </Layout>
+const NotFound = () => ( <
+    Layout >
+    <
+    Seo title = "404 - Not found" / >
+    <
+    Header / >
+    <
+    Container >
+    <
+    Themed.p >
+    Oh, no!
+    <
+    br / >
+    You found a page that doesn 't exist.{` `} <
+    TLink as = { Link }
+    to = "/" >
+    To you want to visit the homepage ?
+    <
+    /TLink>
+    . <
+    /Themed.p> <
+    /Container> <
+    /Layout>
 )
 
 export default NotFound
