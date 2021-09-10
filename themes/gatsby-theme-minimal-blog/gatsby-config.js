1278e791f502946a380ffdedc87779d8bce19db2
gommit <gommit@gmail.com>
2021-09-10 14:54:23 +0200

    Update gatsby-config.js

--- themes/gatsby-theme-minimal-blog/gatsby-config.js
+++ themes/gatsby-theme-minimal-blog/gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = (options) => ({
   siteMetadata: {
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitle: `Gommit`,
+    siteTitleAlt: `Gommit - Finnur's digital garden`,
+    siteHeadline: `Gommit - Finnur's digital garden`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteDescription: `A small patch of the interwebs where Finnur Pálmi Magnússon plants ideas and weeds in the form of disorganised ramblings`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@gommit`,
   },
   plugins: [
     {
