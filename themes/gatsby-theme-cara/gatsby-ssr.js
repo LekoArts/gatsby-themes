7c6ff390bcc91a89a2ab0063a21b7a3dae8bd88b
LekoArts <lekoarts@gmail.com>
2022-09-12 15:37:49 +0200

    update

--- themes/gatsby-theme-cara/gatsby-ssr.js
+++ themes/gatsby-theme-cara/gatsby-ssr.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([<link key="icons-preload" rel="preload" href="/icons.svg" as="image" type="image/svg+xml" />])
 }
