1f4fb5c61bd4029f70be5d9b5874830903eb0911
Lennart <lekoarts@gmail.com>
2022-11-23 11:41:47 +0100

    fix: Use withPrefix helper for SVG icons (#1057)

---
+++ .changeset/new-forks-relax.md
@@ -0,0 +1,6 @@
+---
+"@lekoarts/gatsby-theme-cara": patch
+"@lekoarts/gatsby-theme-emilia": patch
+---
+
+fix: Use withPrefix helper for icons.svg
--- themes/gatsby-theme-cara/gatsby-ssr.js
+++ themes/gatsby-theme-cara/gatsby-ssr.js
@@ -1,5 +1,8 @@
 import React from "react"
+import { withPrefix } from "gatsby"
 
 export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([<link key="icons-preload" rel="preload" href="/icons.svg" as="image" type="image/svg+xml" />])
+  setHeadComponents([
+    <link key="icons-preload" rel="preload" href={withPrefix(`/icons.svg`)} as="image" type="image/svg+xml" />,
+  ])
 }
