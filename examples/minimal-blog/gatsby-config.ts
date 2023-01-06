29ff4eaf60cad9991595bad9e725042ce829746c
LekoArts <lekoarts@gmail.com>
2023-01-06 10:24:06 +0100

    minimal-blog

--- examples/minimal-blog/gatsby-config.ts
+++ examples/minimal-blog/gatsby-config.ts
@@ -1,11 +1,9 @@
-require(`dotenv`).config()
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-module.exports = {
+const config: GatsbyConfig = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
@@ -95,7 +93,11 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allPost } }) =>
+            serialize: ({
+              query: { site, allPost },
+            }: {
+              query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } }
+            }) =>
               allPost.nodes.map((post) => {
                 const url = site.siteMetadata.siteUrl + post.slug
                 const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
@@ -133,5 +135,42 @@ module.exports = {
         openAnalyzer: false,
       },
     },
-  ].filter(Boolean),
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
+
+interface IPostTag {
+  name: string
+  slug: string
+}
+
+interface IPost {
+  slug: string
+  title: string
+  defer: boolean
+  date: string
+  excerpt: string
+  contentFilePath: string
+  html: string
+  timeToRead: number
+  wordCount: number
+  tags: Array<IPostTag>
+  banner: any
+  description: string
+  canonicalUrl: string
+}
+
+interface IAllPost {
+  nodes: Array<IPost>
+}
+
+interface ISiteMetadata {
+  siteTitle: string
+  siteTitleAlt: string
+  siteHeadline: string
+  siteUrl: string
+  siteDescription: string
+  siteImage: string
+  author: string
 }
--- examples/minimal-blog/src/pages/404.tsx
+++ examples/minimal-blog/src/pages/404.tsx
@@ -1,7 +1,8 @@
 import * as React from "react"
+import type { PageProps } from "gatsby"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 
-const NotFound = () => (
+const NotFound = (_props: PageProps) => (
   <Layout>
     <h1>404 - Page Not Found</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
