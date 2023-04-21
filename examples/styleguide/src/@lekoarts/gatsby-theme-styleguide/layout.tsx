51c5b5cd70610500db9c83c7c6acadded9e7eacf
LekoArts <lekoarts@gmail.com>
2023-04-21 14:24:36 +0200

    styleguide: global styles

--- examples/styleguide/src/@lekoarts/gatsby-theme-styleguide/layout.tsx
+++ examples/styleguide/src/@lekoarts/gatsby-theme-styleguide/layout.tsx
@@ -10,18 +10,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
         "*": {
           boxSizing: `border-box`,
         },
-        html: {
-          WebkitTextSizeAdjust: `100%`,
-        },
-        body: {
-          border: 0,
-          margin: 0,
-          padding: 0,
-        },
-        "h1, h2, h3, h4, h5": {
-          marginTop: `4rem !important`,
-          marginBottom: `2rem !important`,
-        },
       })}
     />
     <div sx={{ p: 3, maxWidth: 1024, margin: `0 auto` }}>{children}</div>
