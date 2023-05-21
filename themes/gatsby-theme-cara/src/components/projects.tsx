5a81fbfc3603582de6f179adfe886fc68e00f610
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-05-21 23:52:16 +0700

    improve projects title

--- themes/gatsby-theme-cara/src/components/projects.tsx
+++ themes/gatsby-theme-cara/src/components/projects.tsx
@@ -23,7 +23,7 @@ const Projects = ({ offset, factor = 2 }: { offset: number; factor?: number }) =
             display: `grid`,
             gridGap: [4, 4, 4, 5],
             gridTemplateColumns: [`1fr`, `1fr`, `repeat(2, 1fr)`],
-            h2: { gridColumn: `-1/1`, color: `white !important` },
+            h2: { gridColumn: `-1/1` },
           }}
         >
           <ProjectsMDX />
