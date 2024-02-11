3cc2d8c2482a3ad00ae59b4e96b97c10c8cbfa19
LekoArts <lekoarts@gmail.com>
2024-02-11 11:12:46 +0100

    fix(graphql-playground): Correctly passing through mdx: false

--- themes/gatsby-theme-graphql-playground/utils/default-options.mjs
+++ themes/gatsby-theme-graphql-playground/utils/default-options.mjs
@@ -1,7 +1,7 @@
 export const withDefaults = (themeOptions) => {
   const basePath = themeOptions.basePath || `/`
   const docsPath = themeOptions.docsPath || `docs`
-  const mdx = themeOptions.mdx ?? true
+  const mdx = typeof themeOptions.mdx === `undefined` ? true : themeOptions.mdx
 
   return {
     basePath,
