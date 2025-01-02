ea9215dabc9ebe8d9d4d69e97c289f9eebe0149b
Angie Parra <126300441+gieparra@users.noreply.github.com>
2025-01-02 13:45:09 -0800

    Update gatsby-config.mjs

--- themes/gatsby-theme-jodie/gatsby-config.mjs
+++ themes/gatsby-theme-jodie/gatsby-config.mjs
@@ -1,8 +1,8 @@
 const config = (options) => ({
   siteMetadata: {
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - @lekoarts/gatsby-theme-jodie`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
+    siteTitle: `GieParra`,
+    siteTitleAlt: `Gie - @lekoarts/gatsby-theme-jodie`,
+    siteHeadline: `Gie - Gatsby Theme from @lekoarts`,
     siteUrl: `https://jodie.lekoarts.de`,
     siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
     siteImage: `/banner.jpg`,
